a8b31246a172362702be7d05cac28d0b315f2b2c
Irina Kisel <kiv500220@gmail.com>
2021-07-02 13:14:22 +0300

    проект

--- js/modal.js
+++ js/modal.js
@@ -1,56 +1,25 @@
 const bookingModalForm = document.querySelector(".booking-modal-form");
 const bookingForm = bookingModalForm.querySelector(".booking-form");
-const bookingButtonModal = bookingModalForm.querySelector(".bookingModalForm");
+const bookingButtonModal = bookingModalForm.querySelector(".booking-button-modal");
 const bookingFormDateIn = bookingModalForm.querySelector(".booking-form-date-in");
 const bookingFormDateOut = bookingModalForm.querySelector(".booking-form-date-out");
-const bookingFormDescriptionAdult = bookingModalForm.querySelector(".booking-form-description-adult");
-const bookingFormDescriptionChildren = bookingModalForm.querySelector(".booking-form-description-children");
-
-let isStorageSupport = true;
-let storage = "";
-
-try {
-  storage = localStorage.getItem("adult");
-} catch (err) {
-  isStorageSupport = false;
-}
 
 bookingButtonModal.addEventListener("click", function (evt) {
   evt.preventDefault();
-  bookingForm.classList.toggle("modal-show");
-  bookingForm.classList.toggle("booking-form");
-
-  if (storage) {
-    bookingFormDescriptionAdult.value = storage;
-    bookingFormDescriptionChildren.focus();
-  }
-  else {
-    bookingFormDescriptionAdult.focus();
+  if (!bookingModalForm.open) {
+    bookingModalForm.open = true;
+    bookingFormDateIn.focus();
+  } else {
+    bookingModalForm.open = false;
+    bookingModalForm.focus();
   }
 
 });
-
-// bookingButtonModal.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   bookingForm.classList.remove("modal-show");
-// })
 
 bookingForm.addEventListener("submit", function (evt) {
-  if (!bookingFormDescriptionAdult.value || !bookingFormDescriptionChildren.value) {
+  if (!bookingFormDateIn.value || !bookingFormDateOut.value) {
     evt.preventDefault();
-  }
-  else {
-    if (isStorageSupport) {
-      localStorage.setItem("adult", bookingFormDescriptionAdult.value);
-    }
-  }
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.key === "Escape" || evt.key === "Esc") {
-    if (bookingForm.classList.contains("modal-show")) {
-      evt.preventDefault();
-      bookingForm.classList.remove("modal-show");
-    }
+    bookingForm.classList.add("submit-error");
+    setTimeout(() => { bookingForm.classList.remove("submit-error") }, 600);
   }
 });
